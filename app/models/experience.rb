afe663d68423a506c522a7924118c55e1c52d6fc
Jeremie <jeremie.bonal@gmail.com>
2016-02-29 12:02:16 +0100

    Associations.

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,9 @@
 class Experience < ActiveRecord::Base
   belongs_to :user
 
+  has_many :reviews
+  has_many :users, through: :wishlists
+
   validates :type, presence: true
   validates :description, presence: true
   validates :address, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :experiences
+  has_many :wishlists
+  has_many :wishlisted_experiences, through: :wishlists, source: :experiences
+
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
