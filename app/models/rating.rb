dd704e65b4459fdf49c8df15834fdd8b447ecf3b
Jeremie <jeremie.bonal@gmail.com>
2016-02-29 12:01:55 +0100

    More validations.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,5 @@ class Rating < ActiveRecord::Base
   belongs_to :experience
 
   validates :rating, presence: true
+  validates :user_id, uniqueness: { scope: :experience_id }
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,6 @@
 class Wishlist < ActiveRecord::Base
   belongs_to :user
   belongs_to :experience
+
+  validates :user_id, uniqueness: { scope: :experience_id }
 end
