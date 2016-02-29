be59771e84b2336c601bf66337e92637b0b0bf17
Adelaide de Perlinghi <adedeperl@gmail.com>
2016-02-29 18:19:28 +0100

    autocomplete

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,6 @@
+Geocoder.configure(
+  lookup:    :google,
+  api_key:   ENV['AIzaSyBCoDjqf-9l5d0f0LoOhPQuHSOMaKKP7-0'],
+  use_https: true,
+  units:     :km       # :km for kilometers or :mi for mile
+)
