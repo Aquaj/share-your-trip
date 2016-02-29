9515e8e23fa4d6585be4b476a048d78819830d1c
Jeremie <jeremie.bonal@gmail.com>
2016-02-29 12:15:40 +0100

    Small validation modifications.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,6 @@ class Rating < ActiveRecord::Base
   belongs_to :experience
 
   validates :rating, presence: true
+  validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
   validates :user_id, uniqueness: { scope: :experience_id }
 end
