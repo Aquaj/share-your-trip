6db07462d37b1b0e37e8f12b7110ec57cf9fb8c3
Jeremie <jeremie.bonal@gmail.com>
2016-02-29 11:52:41 +0100

    Validations.

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,7 @@
 class Experience < ActiveRecord::Base
   belongs_to :user
+
+  validates :type, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :experience
+
+  validates :rating, presence: true
 end
