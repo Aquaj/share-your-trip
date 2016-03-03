a2d7e0f2e45ba9405bc124ebe33b16a618939f24
Jeremie <jeremie.bonal@gmail.com>
2016-03-03 17:03:14 +0100

    Validations.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ActiveRecord::Base
   belongs_to :experience
   belongs_to :roadmap
+
+  validates :experience, presence: true
+  validates :roadmap, presence: true
 end
--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -2,4 +2,6 @@ class Roadmap < ActiveRecord::Base
   belongs_to :user
   has_many :activities, dependent: :destroy
   has_many :experiences, through: :activities
+
+  validates :user, presence: true
 end
