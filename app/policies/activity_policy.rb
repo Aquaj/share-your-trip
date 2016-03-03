85dddca4e7357b12bb19a42c14a9301793d6d2d1
Jeremie <jeremie.bonal@gmail.com>
2016-03-03 17:32:49 +0100

    Activities controller + policy (Pundit).

---
+++ app/policies/activity_policy.rb
@@ -0,0 +1,19 @@
+class ActivityPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.joins(:roadmap).where("roadmap.user_id = ?", user.id)
+    end
+  end
+
+  def update?
+    record.roadmap.user == user
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    record.roadmap.user == user
+  end
+end
