5c7a372383d95901d57b07fc31139b84ef6737f8
Jeremie <jeremie.bonal@gmail.com>
2016-04-10 05:16:44 +0200

    Mailing working - Task #26

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "contact@share-your-trip.com"
   add_template_helper(ApplicationHelper)
+  add_template_helper(RoadmapsHelper)
   layout 'mailer'
 end
