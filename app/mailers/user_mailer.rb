03ce0c15ef43f4cc82623b857e64bc92eaced7bf
Jeremie <jeremie.bonal@gmail.com>
2016-04-10 05:06:00 +0200

    Trying SendGrid instead of Postmark - Task #26

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,6 @@ class UserMailer < ApplicationMailer
   #
   def welcome(user)
     @user = user  # Instance variable => available in view
-
     mail(to: @user.email, subject: 'Welcome to Share your Trip')
     # This will render a view in `app/views/user_mailer`!
   end
